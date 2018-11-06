df1240569d6b8c6cf883a4fffc152b3ac2aecfcc
peterliang000 <peter.liang000@gmail.com>
2018-11-06 12:29:59 -0500

    2nd version

---
+++ Learn.js
@@ -0,0 +1,11 @@
+// var btn = document.getElementById("placeInfo")
+
+// btn.onclick = function(){
+// 	var dropdown = document.getElementById("dropdown").value
+
+// result.innerHTML = dropdown + "Manhattan site is at Group Practice Mon"
+// }
+
+function placeInfo(value){
+var info = document.getElementById("result").innerHTML = value;
+}
